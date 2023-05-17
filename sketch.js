1ddd9cc5a518ee34bec2aee24c20b860ebfb04c5
novmk <67250036+novmk@users.noreply.github.com>
2023-05-18 00:15:02 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let objectImages = [];
 let objects = [];
 let gravity = 0.5;
-let bounceReduction = 0.8;
+let bounceReduction = 0.95;
 
 function preload() {
   for (let i = 1; i <= 6; i++) {
@@ -14,18 +14,14 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   
   for (let i = 0; i < 6; i++) {
-    let x = random(100, width-100); // Random horizontal position
+    let x = random(width); // Random horizontal position
     let y = random(-1000, -100); // Random starting position above the canvas
-    let velocity = random(1, 3); // Random falling velocity
+    let size = width / 2; // Image size based on half width of the canvas
+    let speed = 0; // Initial speed
     
-    // Create an object with image, position, velocity, and bounce properties
-    let object = {
-      image: objectImages[i],
-      x: x,
-      y: y,
-      velocity: velocity,
-      isBouncing: false
-    };
+    // Create an object with image, position, speed, and bounce properties
+    let object = new Ball(x, y, size, speed);
+    object.image = objectImages[i];
     
     objects.push(object);
   }
@@ -37,37 +33,39 @@ function draw() {
   for (let i = 0; i < objects.length; i++) {
     let object = objects[i];
     
-    // Update object's position
-    object.y += object.velocity;
+    object.update();
     
     // Check if the object has reached 90% of the window height
     if (object.y >= 0.9 * height) {
-      // If not bouncing, reverse its velocity and set bouncing flag
-      if (!object.isBouncing) {
-        object.velocity *= -bounceReduction;
-        object.isBouncing = true;
-      }
-    }
-    
-    // Check if the object has reached 50% of the window height
-    if (object.isBouncing && object.y >= 0.9 * height) {
-      // Reduce bounce gradually until it comes to a stop
-      object.velocity *= bounceReduction;
-      if (abs(object.velocity) < 0.1) {
-        object.velocity = 0;
-      }
-    }
-    
-    // Apply gravity if not bouncing
-    if (!object.isBouncing) {
-      object.velocity += gravity;
+      object.speed *= -bounceReduction;
     }
     
     // Draw the object image at its current position with half width and auto height
-    let imageSize = width / 5;
+    let imageSize = width / 2;
     let imageHeight = object.image.height * (imageSize / object.image.width);
     image(object.image, object.x, object.y, imageSize, imageHeight);
   }
+}
+
+function Ball(tempX, tempY, tempW, tempSpeed) {
+  this.x = tempX;  // x location of square 
+  this.y = tempY;  // y location of square 
+  this.w = tempW;  // size
+  this.speed = tempSpeed;  // speed
+
+  this.update = function() {
+    // Add speed to location
+    this.y = this.y + this.speed; 
+
+    // Add gravity to speed
+    this.speed = this.speed + gravity; 
+
+    // If square reaches the bottom 
+    // Reverse speed 
+    if (this.y > height) { 
+      this.speed = this.speed * -bounceReduction;  
+    } 
+  };
 }
 
 function windowResized() {
