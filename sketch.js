c02369493c63c7a9dfb0edaa03e16a70ae527970
novmk <67250036+novmk@users.noreply.github.com>
2023-05-17 23:59:44 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let objectImages = [];
 let objects = [];
 let gravity = 0.5;
+let bounceReduction = 0.8;
 
 function preload() {
   for (let i = 1; i <= 6; i++) {
@@ -14,10 +15,10 @@ function setup() {
   
   for (let i = 0; i < 6; i++) {
     let x = random(width); // Random horizontal position
-    let y = random(-200, -100); // Random starting position above the canvas
-    let velocity = random(0.5, 2); // Random falling velocity
+    let y = random(-1000, -100); // Random starting position above the canvas
+    let velocity = random(1, 3); // Random falling velocity
     
-    // Create an object with image, position, velocity, and bounce flag
+    // Create an object with image, position, velocity, and bounce properties
     let object = {
       image: objectImages[i],
       x: x,
@@ -39,12 +40,21 @@ function draw() {
     // Update object's position
     object.y += object.velocity;
     
-    // Check if the object has reached the bottom
-    if (object.y >= height - 100) {
+    // Check if the object has reached 90% of the window height
+    if (object.y >= 0.9 * height) {
       // If not bouncing, reverse its velocity and set bouncing flag
       if (!object.isBouncing) {
-        object.velocity *= -1;
+        object.velocity *= -bounceReduction;
         object.isBouncing = true;
+      }
+    }
+    
+    // Check if the object has reached 50% of the window height
+    if (object.isBouncing && object.y >= 0.5 * height) {
+      // Reduce bounce gradually until it comes to a stop
+      object.velocity *= bounceReduction;
+      if (abs(object.velocity) < 0.1) {
+        object.velocity = 0;
       }
     }
     
