b0e2577e69374f4f3041089ddd71fd7fd64432e8
novmk <67250036+novmk@users.noreply.github.com>
2023-06-05 07:15:10 +0100

    Add files via upload

---
+++ sketch_swans.js
@@ -0,0 +1,56 @@
+let swan;
+let swans = [];
+
+function preload() {
+  swan = loadImage('bread.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  for (var i = 0; i < 23; i++) {
+    var x = random(width);
+    var y = random(height);
+    var speed = random(-1, -0.2);
+  	swans[i] = new newSwan(x, y, speed);
+  }
+
+}
+
+function draw() {
+  background(255);
+  
+  
+  for (var i = 0; i < swans.length; i++) {
+  	swans[i].move();
+    swans[i].display();
+  }
+    if (swans.length > 10) {
+  	swans.shift();
+  }
+  
+}
+
+function newSwan(x, y, speed) {
+  // declare the properties
+  this.xPos = x;
+  this.yPos = y;
+  this.speed = speed;
+}
+
+newSwan.prototype = {
+  constructor: newSwan,
+  display: function() {
+    image(swan, this.xPos, this.yPos, 580, 410);
+  },
+  
+  move: function() {
+    // move down
+	this.xPos += this.speed;
+    // repositon outside of canvas
+    if (this.yPos - this.diameter/2 > height) {
+    	this.yPos = -this.diameter/2;
+    }
+  }
+}
+
