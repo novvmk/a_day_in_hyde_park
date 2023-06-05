e12fc8d3f76527f36463f670252e6f65d000f02e
novmk <67250036+novmk@users.noreply.github.com>
2023-06-05 04:51:13 +0100

    Add files via upload

---
+++ sketch_paul1.js
@@ -0,0 +1,48 @@
+let daisy;
+let shapes = [];
+
+function preload() {
+  daisy = loadImage('images/img_daisy.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+
+  for(let shape of shapes) {
+    push();
+    shape.y += 1;
+    if(shape.shapeType === 'sqaure') {
+      image(daisy, shape.x, shape.y, 180, 180);
+    }
+    if(shape.shapeType === 'ellipse') {
+      image(daisy, shape.x, shape.y, 110, 110);
+    }
+    if(shape.shapeType === 'arc') {
+      image(daisy, shape.x, shape.y, 90, 90);
+    }
+    pop();
+  }
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    let last = shapes.pop();
+  } 
+}
+
+function mousePressed() {
+  addAShape();
+}
+
+function addAShape() {
+  shape1 = {
+    x: mouseX,
+    y: mouseY,
+    shapeType: random(['sqaure', 'ellipse', 'arc'])
+  }
+  shapes.push(shape1);
+}
