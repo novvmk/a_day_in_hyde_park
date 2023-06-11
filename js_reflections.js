f1109197279ea16124f00ed7db101b5a774ecb57
novmk <67250036+novmk@users.noreply.github.com>
2023-06-11 21:22:47 +0100

    Add files via upload

--- js_reflections.js
+++ js_reflections.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 120 && $(document).scrollTop() < 250) {
+    if ($(document).scrollTop() > 150 && $(document).scrollTop() < 260) {
       $("canvas").fadeIn();
     } else {
       $("canvas").fadeOut();
