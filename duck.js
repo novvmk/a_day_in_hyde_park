d06784098efe8f816236361358bde82550f7f9f4
novmk <67250036+novmk@users.noreply.github.com>
2023-06-11 20:22:46 +0100

    Add files via upload

--- duck.js
+++ duck.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   
   $(window).scroll(function() {
-    if ($(document).scrollTop() > 120) {
+    if ($(document).scrollTop() > 200) {
+      console.log(scrollTop);
       $("canvas").css("display", "block");
     } else {
       $("canvas").css("display", "none");
