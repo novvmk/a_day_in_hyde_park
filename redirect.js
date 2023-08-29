b55b11cf12c5705f14cdb391ec66edc234935e6d
novvmk <67250036+novvmk@users.noreply.github.com>
2023-08-29 11:19:51 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,12 +1,1 @@
-var redirectTimeout;
 
-function redirect() {
-    window.location.href = "https://novmk.github.io/csm_show/";
-}
-
-function resetTimer() {
-    clearTimeout(redirectTimeout);
-    redirectTimeout = setTimeout(redirect, 180000); 
-}
-
-document.addEventListener("mousemove", resetTimer);
